9921477fbf58bf298c4829fe6489e64c4dabb46a
olga-adam18 <olgaadam.um@gmail.com>
2026-07-09 23:57:59 +0300

    add POST endpoint for recipes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,jsonify
+from flask import Blueprint,jsonify,request
 import json
 import os
 
@@ -9,4 +9,20 @@ def get_recipes():
     with open(file_path,"r",encoding="utf-8")as f:
         recipes=json.load(f)
 
+    return jsonify(recipes)
+
+@api.route("/recipes", methods=['POST'])
+def add_recipes():
+    recipe = request.get_json()
+    file_path=os.path.join("data","recipes.json")
+
+    with open(file_path,"r",encoding="utf-8") as file:
+        recipes=json.load(file)  
+
+        recipe["id"] = recipes[-1]["id"] + 1 # generate a new id      
+        recipes.append(recipe)
+    
+    with open(file_path,"w",encoding="utf-8") as fl:
+        json.dump(recipes,fl)
+        
     return jsonify(recipes)
