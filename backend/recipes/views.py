04741c7eab88e0f59493963004bd479a402dae55
olga-adam18 <olgaadam.um@gmail.com>
2026-07-16 14:20:31 +0300

    modifying the endpoint

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,7 +1,24 @@
 from django.http import JsonResponse
+from .models import Recipe
 
-def get_recipes(request):
-    return JsonResponse({"message":"the API worked !"})
+def get_list(request):
+
+    recipes = Recipe.objects.all()  #gather all the rows from the Recipe SQlite
+
+    data=[]
+
+    for recipe in recipes:
+        data.append(
+            {
+                "id":recipe.id,
+                "name":recipe.name,
+                "category":recipe.category,
+                "ingredients":recipe.ingredients,
+                "time":recipe.time
+            }
+        )
+
+    return JsonResponse(data,safe=False)
 
 
-# Create your views here.
+
