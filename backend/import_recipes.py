465ecbc5d5829b557eaf07f2b1bf31e2ee4833ce
olga-adam18 <olgaadam.um@gmail.com>
2026-07-16 14:34:28 +0300

    placing all the recipes in sqlite

--- backend/import_recipes.py
+++ backend/import_recipes.py
@@ -13,3 +13,16 @@ with open(file_path, "r", encoding="utf-8") as file:
     recipes = json.load(file)
 
 print(f"Loaded {len(recipes)} recipes.")
+
+if Recipe.objects.exists():
+    print("Rețetele deja există în baza de date")
+else:
+    for recipe in recipes:
+        Recipe.objects.create(
+            name=recipe["name"],
+            category=recipe["category"],
+            ingredients=recipe["ingredients"],
+            time=recipe["time"]
+        )
+    
+    print("Rețetele au fost importate cu succes")
