c516e7d8501ba6d706482c8231115dbb0508d355
olga-adam18 <olgaadam.um@gmail.com>
2026-07-17 13:31:56 +0300

    add route for creating recipes

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_recipes
+from .views import get_recipes,add_recipes
 
 urlpatterns = [
     path("recipes/", get_recipes),
+    path("recipes/add/",add_recipes)
 ]
