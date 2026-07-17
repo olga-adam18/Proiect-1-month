cb9313befd43cb3c61b667935a9e74b877bf3863
olga-adam18 <olgaadam.um@gmail.com>
2026-07-17 17:15:40 +0300

    the path for updating recipes

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_recipes,add_recipes
+from .views import get_recipes,add_recipes,update_recipes
 
 urlpatterns = [
     path("recipes/", get_recipes),
-    path("recipes/add/",add_recipes)
+    path("recipes/add/",add_recipes),
+    path("recipes/update/<int:id>/", update_recipes),
 ]
