3fcc509e263afc309064222f4b247ae99096e427
olga-adam18 <olgaadam.um@gmail.com>
2026-07-29 12:15:07 +0300

    changed some names

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -2,16 +2,15 @@ from django.urls import path
 from .views import (
     get_recipes,
     add_recipes,
-    update_recipes,
-    patch_recipes,
-    delete_recipes,
+    update_recipes,delete_recipes,
+    change_recipes_entirely,
     get_one_recipe )
 
 urlpatterns = [
     path("recipes/", get_recipes),
     path("recipes/add/",add_recipes),
-    path("recipes/update/<int:id>/", update_recipes),
-    path("recipes/patch/<int:id>/", patch_recipes),
+    path("recipes/patch/<int:id>/", update_recipes),
+    path("recipes/put/<int:id>/", change_recipes_entirely),
     path("recipes/delete/<int:id>/",delete_recipes),
     path("recipes/<int:id>/",get_one_recipe)
 ]
