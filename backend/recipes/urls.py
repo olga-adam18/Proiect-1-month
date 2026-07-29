59cd1f526324ef9ceb43a7cebbc3ed0b08e869af
olga-adam18 <olgaadam.um@gmail.com>
2026-07-29 15:20:28 +0300

    the paths for nutritions and products

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -4,13 +4,26 @@ from .views import (
     add_recipes,
     update_recipes,delete_recipes,
     change_recipes_entirely,
-    get_one_recipe )
+    get_one_recipe ,
+    get_products,
+    get_one_product,
+    get_nutritions,
+    get_one_nutrition)
 
 urlpatterns = [
+    #recipes 
     path("recipes/", get_recipes),
     path("recipes/add/",add_recipes),
     path("recipes/patch/<int:id>/", update_recipes),
     path("recipes/put/<int:id>/", change_recipes_entirely),
     path("recipes/delete/<int:id>/",delete_recipes),
-    path("recipes/<int:id>/",get_one_recipe)
+    path("recipes/<int:id>/",get_one_recipe),
+
+    #products
+    path("products/",get_products),
+    path("products/<int:id>/",get_one_product),
+
+    #nutritions
+    path("nutritions/",get_nutritions),
+    path("nutritions/<int:id>/",get_one_nutrition)
 ]
