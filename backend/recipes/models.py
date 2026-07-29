36a17c91eb2f99a36ff7f01ce5bdda713deba94d
olga-adam18 <olgaadam.um@gmail.com>
2026-07-29 12:20:34 +0300

    new class:Products and Nutritions

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -2,12 +2,37 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+#model for Recipes
 class Recipe(models.Model):
     name=models.CharField(max_length=100)
     category = models.CharField(max_length=50)
     ingredients=models.JSONField()
     time=models.CharField(max_length=50)
     is_default=models.BooleanField(default=False)
-    
+
+#model for Products
+class Product(models.Model):
+    name=models.CharField(max_length=70)
+    category=models.CharField(max_length=50)
+    calories=models.CharField(max_length=20)
+    protein = models.CharField(max_length=20)
+    fat = models.CharField(max_length=20)
+    carbohydrates=models.CharField(max_length=20)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.name
+
+#model for Nutrition
+class Nutrition(models.Model):
+    name=models.CharField(max_length=20)
+    category=models.CharField(max_length=20)
+    description=models.TextField()
+    daily_intake=models.TextField()
+    sources=models.JSONField()
+
+    def __str__(self):
+        return self.name
 
 
